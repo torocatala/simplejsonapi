f1fe5c58d6772ae00b4f424922c6567cf05de31f
Ivan Jimenez <torocatala@gmail.com>
2023-11-17 09:25:24 +0100

    Adds idempotency to start.sh

--- interviewsolution/start.sh
+++ interviewsolution/start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-docker compose build --no-cache
+docker compose down --volumes --remove-orphans
+
+#docker compose build --no-cache
+docker compose build
 
 docker compose up --pull always -d --wait
 
 echo "Project running in https://localhost"
 
 echo "Run docker compose down --remove-orphans to kill the project"
+
+docker compose logs -f
